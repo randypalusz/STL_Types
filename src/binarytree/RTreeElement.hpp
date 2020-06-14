52adf8bcedd52631afbc4c6f179bb3da3a3a5206
randypalusz <ibanezguy998@gmail.com>
2020-06-14 14:56:28 -0400

    Added skeleton for binarytree, code for TreeElement

---
+++ src/binarytree/RTreeElement.hpp
@@ -0,0 +1,31 @@
+#ifndef RTreeElement  //NOLINT
+#define RTreeElement 
+
+// Copyright 2020 <randypalusz>
+
+namespace rp {
+template <typename T>
+class TreeElement {
+ public:
+  explicit TreeElement(T data) {
+		data_ = data;
+		left_ = nullptr;
+		right_ = nullptr;
+		parent = nullptr;
+	}
+	~TreeElement() = default;
+	auto getData() -> T { return data_; }
+	auto getLeft() -> TreeElement* { return left_; }
+	auto getRight() -> TreeElement* { return right_; }
+	void setLeft(TreeElement* left) { left_ = left; }
+  void setRight(TreeElement* right) { right_ = right; }
+	void updateData(T data) { data_ = data; }	
+ private:
+	 TreeElement* left_;
+	 TreeElement* right_;
+	 TreeElement* parent_;
+	 T data_;
+};
+}  // namespace rp
+
+#endif  // RTreeElement //NOLINT
